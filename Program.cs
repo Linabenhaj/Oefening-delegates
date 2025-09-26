d8beb5b59f99c320d7d333740d73ef01ff582e41
Linabenhaj <Benhaj.L@outlook.com>
2025-09-26 16:12:42 +0200

    STappenplan voor alarm app

--- Program.cs
+++ Program.cs
@@ -7,7 +7,20 @@ class Program
         bool running = true;
         while (running)
         {
-           
+
+            Console.WriteLine("\n--- WEKKER MENU ---");
+            Console.WriteLine("1. Stel alarmtijd in (seconden)");
+            Console.WriteLine("2. Stel sluimertijd in (seconden)");
+            Console.WriteLine("3. Voeg geluid toe");
+            Console.WriteLine("4. Voeg boodschap toe");
+            Console.WriteLine("5. Voeg knipperlicht toe");
+            Console.WriteLine("6. Start wekker");
+            Console.WriteLine("7. Stop wekker");
+            Console.WriteLine("8. Sluimer");
+            Console.WriteLine("0. Afsluiten");
+            Console.Write("Kies optie: ");
+
+            string choice = Console.ReadLine();
             switch (choice)
             {
                 case "0":
