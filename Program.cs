6097c11ffcff2383f99387f690710e3538e55d03
Linabenhaj <Benhaj.L@outlook.com>
2025-09-26 16:09:56 +0200

    Initial commit: Add base alarm project with delegates

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool running = true;
+        while (running)
+        {
+           
+            switch (choice)
+            {
+                case "0":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Functie nog niet geïmplementeerd.");
+                    break;
+            }
+        }
+    }
+}
